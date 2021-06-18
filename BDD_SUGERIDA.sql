22542f1dd66a748b1b17866a8e531d458e890758
Emmanuel <flotuz10@gmail.com>
2021-06-17 22:24:59 -0500

    BD FINAL v1

--- BDD_SUGERIDA.sql
+++ BDD_SUGERIDA.sql
@@ -34,31 +34,12 @@ CREATE TABLE IF NOT EXISTS `core`.`aspel` (
   `UUID` VARCHAR(50) NULL DEFAULT NULL COMMENT 'ID para sincronización con SAE Movil',
   `CVE_UNIDAD` VARCHAR(4) NULL DEFAULT NULL COMMENT 'Clave de la unidad SAT',
   `CVE_ALT` VARCHAR(45) NULL COMMENT 'Clave alterna SKU',
-  `CAMP_LIBRE1` VARCHAR(100) NULL,
-  `CAMP_LIBRE2` VARCHAR(100) NULL,
-  `CAMP_LIBRE3` VARCHAR(100) NULL,
-  `CAMP_LIBRE4` VARCHAR(100) NULL,
-  `CAMP_LIBRE5` VARCHAR(100) NULL,
-  `CAMP_LIBRE6` VARCHAR(100) NULL,
-  `CAMP_LIBRE7` VARCHAR(100) NULL,
-  `PRECIO_VTA` DOUBLE NOT NULL COMMENT 'Precio de venta en SAP',
-  `PRECIO_VTA_ML` DOUBLE NOT NULL COMMENT 'Precio de venta en mercado libre',
-  `PRECIO_VTA_AMZ` DOUBLE NOT NULL COMMENT 'Precio de venta en AMAZON',
-  `PRECIO_VTA_WWC` DOUBLE NOT NULL COMMENT 'Precio venta WooCommerce',
-  `P_ML` TINYINT NULL DEFAULT 0 COMMENT 'Publicado en mercado libre? [0]: Si   [1]: No',
-  `P_AMZ` TINYINT NULL DEFAULT 0 COMMENT 'Publicado en Amazon? [0]: Si   [1]: No',
-  `P_WWC` TINYINT NULL DEFAULT 0 COMMENT 'Publicado en wooCommerce? [0]: Si   [1]: No',
-  `DESC_LARGA` TEXT NULL DEFAULT 'no existe descripcion para este producto',
-  `STATUS` VARCHAR(1) NULL DEFAULT NULL COMMENT 'Estatus [A/B] .: A=Activo, B=Baja',
-  `COSTO_PROM` DOUBLE NULL DEFAULT NULL COMMENT 'Costo promedio {0.0 ..}',
-  `STOCK_MIN` DOUBLE NULL DEFAULT NULL COMMENT 'Stock mínimo {0.0 .. }',
-  `STOCK_MAX` DOUBLE NULL DEFAULT NULL COMMENT 'Stock máximo {0.0 ..}',
-  `FCH_ULTVTA` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Fecha de última venta',
-  `TIPO_ELE` VARCHAR(1) NULL DEFAULT NULL COMMENT 'Tipo de elemento [P/K/G/S] .: P=Producto, K=Kits, G=Grupo, S=Servicio',
-  `PESO` DOUBLE NULL DEFAULT NULL COMMENT 'Peso {0.0 ..}',
-  `VOLUMEN` DOUBLE NULL DEFAULT NULL COMMENT 'Volumen {0.0..}',
-  `BLK_CST_EXT` VARCHAR(1) NULL DEFAULT NULL COMMENT 'Bloqueado por costos-existencias. [S/N] .: S=Si, N=No',
-  `F_CREACION` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT 'Fecha creación de la publicación',
+  `CAMPLIB3` VARCHAR(100) NULL,
+  `CAMPLIB4` VARCHAR(100) NULL,
+  `CAMPLIB5` VARCHAR(100) NULL,
+  `PRECIO6` VARCHAR(100) NULL,
+  `CAMPO_LIBRE7` VARCHAR(100) NULL,
+  `CAMPO_LIBRE8` VARCHAR(100) NULL,
   PRIMARY KEY (`idProducto`),
   UNIQUE INDEX `idProducto_UNIQUE` (`idProducto` ASC) VISIBLE)
 ENGINE = InnoDB
@@ -120,6 +101,55 @@ CREATE TABLE IF NOT EXISTS `core`.`notificicaciones` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `core`.`productos_publicados`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `core`.`productos_publicados` (
+  `idProductoPublicado` INT NOT NULL AUTO_INCREMENT,
+  `CVE_ART` VARCHAR(20) NOT NULL COMMENT 'Clave del articulo',
+  `DESCR` VARCHAR(40) NULL DEFAULT NULL COMMENT 'Descripción',
+  `EXIST` DOUBLE NULL DEFAULT NULL COMMENT 'Existencias {0.0 ..}',
+  `LIN_PROD` VARCHAR(5) NULL DEFAULT NULL COMMENT 'Clave de línea',
+  `UNI_MED` VARCHAR(10) NULL DEFAULT NULL COMMENT 'Unidad de entrada',
+  `NUM_MON` INT(11) NULL DEFAULT NULL COMMENT 'Clave de moneda',
+  `FCH_ULTCOM` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP() COMMENT 'Fecha de última compra',
+  `ULT_COSTO` DOUBLE NULL DEFAULT NULL COMMENT 'Último costo {0.0 ..}',
+  `CVE_IMAGEN` VARCHAR(16) NULL DEFAULT NULL COMMENT 'Nombre de la imagen',
+  `UUID` VARCHAR(50) NULL DEFAULT NULL COMMENT 'ID para sincronización con SAE Movil',
+  `CVE_UNIDAD` VARCHAR(4) NULL DEFAULT NULL COMMENT 'Clave de la unidad SAT',
+  `CVE_ALT` VARCHAR(45) NULL COMMENT 'Clave alterna SKU',
+  `CAMP_LIBRE1` VARCHAR(100) NULL,
+  `CAMP_LIBRE2` VARCHAR(100) NULL,
+  `CAMP_LIBRE3` VARCHAR(100) NULL,
+  `CAMP_LIBRE4` VARCHAR(100) NULL,
+  `CAMP_LIBRE5` VARCHAR(100) NULL,
+  `CAMP_LIBRE6` VARCHAR(100) NULL,
+  `CAMP_LIBRE7` VARCHAR(100) NULL,
+  `PRECIO_VTA` DOUBLE NOT NULL COMMENT 'Precio de venta en SAP',
+  `PRECIO_VTA_ML` DOUBLE NOT NULL COMMENT 'Precio de venta en mercado libre',
+  `PRECIO_VTA_AMZ` DOUBLE NOT NULL COMMENT 'Precio de venta en AMAZON',
+  `PRECIO_VTA_WWC` DOUBLE NOT NULL COMMENT 'Precio venta WooCommerce',
+  `P_ML` TINYINT NULL DEFAULT 0 COMMENT 'Publicado en mercado libre? [0]: Si   [1]: No',
+  `P_AMZ` TINYINT NULL DEFAULT 0 COMMENT 'Publicado en Amazon? [0]: Si   [1]: No',
+  `P_WC` TINYINT NULL DEFAULT 0 COMMENT 'Publicado en wooCommerce? [0]: Si   [1]: No',
+  `DESC_LARGA` TEXT NULL,
+  `STATUS` VARCHAR(1) NULL DEFAULT NULL COMMENT 'Estatus [A/B] .: A=Activo, B=Baja',
+  `COSTO_PROM` DOUBLE NULL DEFAULT NULL COMMENT 'Costo promedio {0.0 ..}',
+  `STOCK_MIN` DOUBLE NULL DEFAULT NULL COMMENT 'Stock mínimo {0.0 .. }',
+  `STOCK_MAX` DOUBLE NULL DEFAULT NULL COMMENT 'Stock máximo {0.0 ..}',
+  `FCH_ULTVTA` DATETIME NOT NULL COMMENT 'Fecha de última venta',
+  `TIPO_ELE` VARCHAR(1) NULL DEFAULT NULL COMMENT 'Tipo de elemento [P/K/G/S] .: P=Producto, K=Kits, G=Grupo, S=Servicio',
+  `PESO` DOUBLE NULL DEFAULT NULL COMMENT 'Peso {0.0 ..}',
+  `VOLUMEN` DOUBLE NULL DEFAULT NULL COMMENT 'Volumen {0.0..}',
+  `BLK_CST_EXT` VARCHAR(1) NULL DEFAULT NULL COMMENT 'Bloqueado por costos-existencias. [S/N] .: S=Si, N=No',
+  `LINK_A_TIENDA` TEXT NULL COMMENT 'URL de del producto en tienda',
+  `F_CREACION` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT 'Fecha creación de la publicación',
+  PRIMARY KEY (`idProductoPublicado`),
+  UNIQUE INDEX `idProducto_UNIQUE` (`idProductoPublicado` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
